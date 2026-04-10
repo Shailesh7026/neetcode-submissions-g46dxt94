79b16935d32b0f4ead1caaf936b22b5092b3590c
Shailesh <103130580+Shailesh7026@users.noreply.github.com>
2026-04-10 14:41:08 +0530

    Add: move-zeroes - submission-4

---
+++ Data Structures & Algorithms/move-zeroes/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int j=0;
+        while(j<nums.length){
+            if(nums[j] != 0){
+                nums[i] = nums[j];
+                if(i!=j) nums[j] = 0;
+                i++;
+            }
+            j++;
+        }
+
+        // while(i<nums.length){
+        //     nums[i] = 0;
+        //     i++;
+        // }
+    }
+}
